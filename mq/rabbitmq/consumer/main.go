dcbd67241ae44968129cf94e12a1492cdaf706c6
hrh <huangronghua@yctc.tech>
2023-03-22 10:39:35 +0800

    操作rabbitmq demo

---
+++ mq/rabbitmq/consumer/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func main() {
+	conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	defer conn.Close()
+
+	ch, _ := conn.Channel()
+	defer ch.Close()
+
+	q, _ := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	msgCh, _ := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	go func() {
+		log.Println("consumer start !")
+		for d := range msgCh {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	select {}
+}
---
+++ mq/rabbitmq/producer/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func main() {
+	conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	defer conn.Close()
+
+	ch, _ := conn.Channel()
+	defer ch.Close()
+
+	q, _ := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+
+	body := "Hello World!"
+	err := ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	if err != nil {
+		log.Println("Publish a message err: ", err.Error())
+	}
+	log.Println("Publish a message:", body)
+}
